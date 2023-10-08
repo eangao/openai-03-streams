70ad1ea69831eaa087c75841d7a77ebfd82b3a4c
eangao <elmarcera@gmail.com>
2023-10-08 17:16:24 +0800

    Introduction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
+import Stream from "./screens/Stream";
 import "./styles/bootstrap-custom.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Stream />}></Route>
         </Routes>
       </Router>
     </>
